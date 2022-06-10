338915a7682068333772afbe60af4294599c878c
mrjulia22 <maria.jaraujo@ufpe.br>
2022-06-09 21:29:22 -0300

    impletação de post de postagens

--- src/app/model/Postagem.ts
+++ src/app/model/Postagem.ts
@@ -1,7 +1,7 @@
 import { Tema } from "./Tema"
 import { User } from "./User"
 
-export class Postagem {
+export class Postagem{
     public id: number
     public titulo: string
     public texto: string
